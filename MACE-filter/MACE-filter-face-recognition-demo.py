5d96978c7141065c9606534802f0cad9993e4064
Chris D <34282557+chrisdcs@users.noreply.github.com>
2020-12-21 16:46:18 -0500

    A demo for MACE filter

---
+++ MACE-filter/MACE-filter-face-recognition-demo.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 16:14:49 2020
+
+@author: Chris Ding
+"""
+import cv2
+import numpy as np
+import os
+
+from utils.MACE import MACE
+from utils.tools import plot_correlation_plane
+#%% Load Data
+trainD = np.zeros((10,64,64)).astype(np.uint8)
+# print(os.getcwd())
+
+for i in range(10):
+    trainD[i] = cv2.imread(r'MACE-filter\faceExpressionDatabase\A'+ str(i // 10) + str(i % 10) + '.bmp',-1)
+    
+testD = np.zeros((60,64,64)).astype(np.uint8)
+for i in range(60):
+    testD[i] = cv2.imread(r'MACE-filter\faceExpressionDatabase\D' + str(i // 10) + str(i % 10) + '.bmp',-1)    
+#%%
+maceA = MACE(trainD)
+maceA.train()
+
+maceB = MACE(testD)
+maceB.train()
+#%% plot things
+
+Corr,peak = maceA.test(testD[4],compute_Corr = True)
+
+plot_correlation_plane(Corr,title_name='MACE Filter False')
+
+
+Corr1,peak1 = maceB.test(testD[4],compute_Corr = True)
+
+# Plot the surface.
+plot_correlation_plane(Corr1,title_name='MACE Filter True')
+
+print("False image center correlation:\n",peak)
+print("True image center correlation:\n",peak1)
